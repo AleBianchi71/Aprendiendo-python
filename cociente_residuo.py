9eb3f43a0b2894dbbbafdcb281d007a628c36728
AleBianchi71 <bianchialejandro998@gmail.com>
2023-04-12 11:35:55 -0300

    cociente y residuo

---
+++ cociente_residuo.py
@@ -0,0 +1,13 @@
+print('Calcular cociente y residuo')
+
+a = input('ingrese primer numero: ')
+b = input('ingrese segundo numero: ')
+
+a = int(a)
+b = int(b)
+
+cociente = a // b
+residuo = a%b
+
+print('cociente: ', cociente)
+print('residuo: ',residuo)
