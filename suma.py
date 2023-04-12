d8358ff048d44bc587348d9625b0d660e0f76431
AleBianchi71 <bianchialejandro998@gmail.com>
2023-04-12 11:23:48 -0300

    suma de numeros

---
+++ suma.py
@@ -0,0 +1,15 @@
+print('suma de dos numeros enteros')
+
+# Entrada de datos
+a = input('ingrese primer numero: ')
+b = input('ingrese segundo numero: ')
+
+# Operación
+a = int(a)
+b = int(b)
+
+suma = a+b
+
+
+# salida
+print("la suma es: ", suma)
