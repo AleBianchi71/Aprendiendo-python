193c1e3aaf8134a82d652f650fe733c44a48384d
AleBianchi71 <bianchialejandro998@gmail.com>
2023-04-12 11:55:01 -0300

    actualización de venta

--- venta.py
+++ venta.py
@@ -1,11 +1,17 @@
-print('Precio de venta')
+print('---- REALIZAR UNA VENTA ----')
 
 
-venta = float(input('ingrese el calor de venta: $'))
+venta = float(input('ingrese el valor de venta: $'))
 
 igv = venta * 0.18
 
 precio = venta + igv
 
-print('El precio de venta es: $',precio)
+print('='*25)
+print('---- FACTURA DE VENTA ----')
+print('='*25)
+print('valor de venta: $',venta)
+print('IVA: $',igv)
+print('TOTAL: $',precio)
+print('='*25)
 
